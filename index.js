02310bd525daeb4ef7e75b951f239d57f1b7abb9
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-02-27 21:19:49 +0600

    connect with vercel

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 const port = process.env.PORT || 5000;
 
 //middleware
-app.use(cors());
+app.use(cors({origin: ['http://localhost:5173','https://ks-automotive-limited.web.app','https://ks-automotive-limited.firebaseapp.com']}));
 app.use(express.json());
 
 
